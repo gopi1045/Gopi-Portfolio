96bc2b5cb8fa7f5772791f5edc3c37a106f2eb5c
gopi1045 <hardikgopi1045@gmail.com>
2024-09-29 13:51:21 +0530

    Downloading resume from drive

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import CV from '../../assets/Resume_Gopi.pdf'
+// import CV from '../../assets/Resume_Gopi.pdf'
 
 const CTA = () => {
+  let CV="https://drive.google.com/file/d/17A6Ux-CaVDP2kGvIpWNzXL7HojHigkIC/view?usp=drive_link";
   return (
     <div className="cta">
         <a href={CV} download className='btn'>Download CV</a>
